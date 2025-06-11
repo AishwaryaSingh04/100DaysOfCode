1aa903b68f312e7c3261c0a5d878d2d6eeda5368
Aishwarya Singh <aishkashyap04@gmail.com>
2025-06-10 22:57:56 -0700

    Update Pattern_Practice

--- Pattern_Practice.js
+++ Pattern_Practice.js
@@ -9,9 +9,9 @@
  */
 
 
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 4; i++) {
     let row = " "
-    for (let j = 0; j < 4; j++){
+    for (let j = 0; j < 4; j++) {
         row = row + " *"
     }
     console.log(row)
@@ -32,7 +32,7 @@ for (let i = 0; i < 4; i++){
 
 for (let i = 0; i < 4; i++) {
     let row = " ";
-    for (let j = 0; j <= i; j++){
+    for (let j = 0; j <= i; j++) {
         row = row + "*";
     }
     console.log(row)
@@ -41,3 +41,74 @@ for (let i = 0; i < 4; i++) {
 
 
 /***************************** */
+
+/**
+ 3rd Pattern
+ 1
+ 2 2
+ 3 3 3 3
+ 4 4 4 4 4
+ */
+
+for (let i = 1; i < 5; i++) {
+    let row = " ";
+    for (let j = 1; j <= i; j++) {
+        row = row + i;
+    }
+    console.log(row);
+}
+
+/*************************************** */
+
+
+/**
+ 4rth Pattern
+ 1 2 3 4 5
+ 1 2 3 4
+ 1 2 3
+ 1 2
+ 1
+ */
+
+for (let i = 1; i < 6; i++) {
+    let row = " ";
+    for (let j = 1; j <= 6 - i; j++) {
+        row = row + " " + j;
+    }
+    console.log(row)
+
+}
+
+/*************************************** */
+
+/**
+ 5th Pattern
+
+      *
+    * * 
+  * * * 
+* * * *
+
+ */
+
+for (let i = 1; i < 6; i++) {
+    let row = " ";
+    for (let j = 1; j <= 6 - i; j++) {
+        if (j == i) {
+            row = row + '*'
+        } else {
+            row = row + " ";
+        }
+    }
+    console.log(row)
+
+}
+
+
+
+
+
+
+
+
+/***************************************************** */
